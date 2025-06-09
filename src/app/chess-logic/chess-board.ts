6f06918c5993e5a22bcd440914c97cd62173b103
Arashi-Crow <ignaciofdce92@gmail.com>
2025-06-09 08:42:37 -0600

    init pieces and chess-board

---
+++ src/app/chess-logic/chess-board.ts
@@ -0,0 +1,41 @@
+
+import { Color } from "./models";
+import { Bishop } from "./pieces/bishop";
+import { King } from "./pieces/king";
+import { Knight } from "./pieces/knight";
+import { Pawn } from "./pieces/pawn";
+import { Piece } from "./pieces/piece";
+import { Queen } from "./pieces/queen";
+import { Rook } from "./pieces/rook";
+
+export class ChessBoard {
+    private chessBoard: (Piece | null)[][];
+    private readonly chessBoardSize: number = 8;
+    private _playerColor = Color.White;
+
+    constructor() {
+        this.chessBoard = [
+            [
+                new Rook(Color.White), new Knight(Color.White), new Bishop(Color.White), new Queen(Color.White),
+                new King(Color.White), new Bishop(Color.White), new Knight(Color.White), new Rook(Color.White)
+            ],
+            [
+                new Pawn(Color.White), new Pawn(Color.White), new Pawn(Color.White), new Pawn(Color.White),
+                new Pawn(Color.White), new Pawn(Color.White), new Pawn(Color.White), new Pawn(Color.White)
+            ],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [
+                new Pawn(Color.Black), new Pawn(Color.Black), new Pawn(Color.Black), new Pawn(Color.Black),
+                new Pawn(Color.Black), new Pawn(Color.Black), new Pawn(Color.Black), new Pawn(Color.Black)
+            ],
+            [
+                new Rook(Color.Black), new Knight(Color.Black), new Bishop(Color.Black), new Queen(Color.Black),
+                new King(Color.Black), new Bishop(Color.Black), new Knight(Color.Black), new Rook(Color.Black)
+            ],
+        ];
+    }
+
+}
